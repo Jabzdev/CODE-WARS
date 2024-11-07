c2defb204e364e1868d1eb250511a4a5d4b48e47
Jabu Himo <abduljabarcodes@gmail.com>
2024-11-07 20:46:08 +0300

    added new codewars challenge

--- 7KYU/isograms.js
+++ 7KYU/isograms.js
@@ -11,20 +11,5 @@ Example: (Input --> Output)
 //My solution
 //#1
 function isIsogram(str) {
-  str = str.toLowerCase();
-
-  // Create a Set to store unique characters
-  let charSet = new Set();
-
-  // Loop through each character in the string
-  for (let char of str) {
-    if (charSet.has(char)) {
-      // If the character is already in the set, it's a repeating character
-      return false;
-    }
-    charSet.add(char); // Add the character to the set
-  }
-
-  // If no repeating characters were found, it's an isogram
-  return true;
+  return new Set(str.toUpperCase()).size == str.length;
 }
