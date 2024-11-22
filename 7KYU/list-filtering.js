69ab3f2b363b71b4ee118705596efa0c181523e1
Jabu Himo <abduljabarcodes@gmail.com>
2024-11-22 21:51:42 +0300

    added new codewars challenge

--- 7KYU/list-filtering.js
+++ 7KYU/list-filtering.js
@@ -10,7 +10,10 @@ filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 //My solution
 //#1
 function filter_list(l) {
-  // Return a new array with the strings filtered out
-
   return l.filter((x) => typeof x === "number");
+}
+
+//#2
+function filter_list(l) {
+  return l.filter(Number.isInteger);
 }
