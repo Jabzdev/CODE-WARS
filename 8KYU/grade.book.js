867651841c897cb1fd640dde0871f34be92cd87f
Jabu Himo <abduljabarcodes@gmail.com>
2024-11-23 19:41:47 +0300

    added new codewars challenge

--- 8KYU/grade.book.js
+++ 8KYU/grade.book.js
@@ -29,3 +29,9 @@ function getGrade(s1, s2, s3) {
     return null;
   }
 }
+
+//#2
+function getGrade(s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3;
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F";
+}
